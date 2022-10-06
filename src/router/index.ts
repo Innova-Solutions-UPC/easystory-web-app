9606fdc0152a3d061d8dddef8f559b875afd3b31
Sebastian Toulier <sebaas14toulier@gmail.com>
2022-10-06 11:13:26 -0500

    refactor: update router paths

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import ProfileView from '../views/profile/Profile.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -20,12 +21,12 @@ const router = createRouter({
     {
       path:'/login',
       name: 'Login',
-      component: () => import ('../views/login/Login.vue')
+      component: () => import ('@/views/login/Login.vue')
     },
     {
       path: '/profile',
       name: 'Profile',
-      component: () => import('@/views/profile/Profile.vue')
+      component: ProfileView
     }
   ]
 })

ea1dcaf7ca6e31be1e071042ae0f5f556a1620f5
Sebastian Toulier <sebaas14toulier@gmail.com>
2022-10-31 12:38:44 -0500

    refactor: move item entity bc

--- src/publishing/model/publishing.facade.ts
+++ src/publishing/model/publishing.facade.ts
@@ -2,7 +2,7 @@ import {PublishingApiServices} from "@/publishing/service/publishing-api.service
 import {reactive} from "vue";
 import type {CreatePostInterface} from "@/publishing/model/entities/create-post.interface";
 import type {AuthorPosts} from "@/publishing/model/entities/author-posts.interface";
-import type {Item} from "@/publishing/model/entities/item.interface";
+import type {Item} from "@/shared/models/entities/item.interface";
 import appController from "@/shared/models/Controller";
 
 
